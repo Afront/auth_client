8fe2e0b0e38b43ff075c1ae517b5a7954d675edd
Afront <3943720+Afront@users.noreply.github.com>
2020-01-03 23:57:29 +0900

    Tried switching to async/await...
    I give up
    Will switch to quiche...

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::io;
+use std::{env, io};
 use std::io::Write;
 use std::process;
 use argonautica::Hasher;
+use reqwest::Response;
 use rpassword::read_password;
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
+//use serde_json::{Result};
 
 #[derive(Serialize, Deserialize)]
 struct User {
@@ -12,6 +13,17 @@ struct User {
 	email: String,
 	password: String,
 }
+
+#[derive(Debug)]
+enum LoginResult {
+	AlreadyQuited(bool),
+	AuthCode(String),
+	Error(String),
+	Helped(bool),
+	SignedUp(bool),
+}
+
+
 
 
 fn hash(password: String) -> String {
@@ -26,24 +38,44 @@ fn hash(password: String) -> String {
 	hash
 }
 
-fn abort() {
+fn abort() -> Result<LoginResult, LoginResult>{
 	println!("AHHHHHHHHH");
 	process::exit(0);
+	let x: Result<LoginResult, LoginResult> = Ok(LoginResult::AlreadyQuited(true));
+	x //since the compiler will complain...
 }
 
-fn help() {
+fn help() -> Result<LoginResult, LoginResult> {
 	println!("You need help? I don't think I'm the right person to ask. Try calling someone on your phone.");
 	abort();
+	let x: Result<LoginResult, LoginResult> = Ok(LoginResult::Helped(true));
+	x //since the compiler will complain...
 }
 
-fn signin() {
+fn signin() -> Result<LoginResult, LoginResult> {
 	println!("You want to sign in? Well, not today.");
 	abort();
+	let x: Result<LoginResult, LoginResult> = Ok(LoginResult::AuthCode(String::from("some_auth_code")));
+	x //since the compiler will complain...
 }
 
+async fn send_json(user_json: serde_json::Result<String>) -> reqwest::Result<Response> {
+	let client = reqwest::Client::new();
+	let server_url = env::var("SERVER_URL").expect("SERVER_URL must be set");
 
+	let res = client.post(&server_url)
+		.body("hi!")
+		.send()
+		.await?;
 
-fn signup() {
+	println!("{:?}", &user_json);
+	println!("{:?}", &res);
+
+	let x: reqwest::Result<Response> = Ok(res);
+	x
+}
+
+async fn signup() -> Result<LoginResult, LoginResult> {
 	loop {
 		print!("\x1B[2J");
 		let mut id = String::new();
@@ -73,12 +105,15 @@ fn signup() {
 			};
 			let user_json = serde_json::to_string(&user);
 			println!("{:?}", user_json);
+			send_json(user_json).await;
 			break;
 		}
 	}
+	let x: Result<LoginResult, LoginResult> = Ok(LoginResult::SignedUp(true));
+	x
 }
 
-fn login_screen(){
+async fn login_screen() -> io::Result<()>{
 	let mut not_authenticated = true;
 	while not_authenticated {
 		print!("\x1B[2J");
@@ -90,7 +125,7 @@ fn login_screen(){
 		match input.trim().to_uppercase().as_str() {
 				"ABORT" | "EXIT" | "Q" | "QUIT" => abort(),
 				"HELP" | "H" => help(),
-				"SIGN UP" | "SIGNUP" | "REGISTER" | "R" => signup(),
+				"SIGN UP" | "SIGNUP" | "REGISTER" | "R" => signup().await,
 				"SIGN IN" | "SIGNIN" | "LOGIN" | "LOG IN" | "S" => signin(),
 				_  => continue,
 		};
@@ -98,13 +133,15 @@ fn login_screen(){
 			not_authenticated = false;
 		}
 	}
+	Ok(())
 }
 
+/**
 fn do_something(){
 
 }
+**/
 
-fn main() {
-	login_screen();
-	do_something();
+async fn main() -> io::Result<()> {
+	login_screen().await
 }
