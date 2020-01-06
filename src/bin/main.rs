8838873e6e888ff91ec4f4f12749c8f224613c72
Afront <3943720+Afront@users.noreply.github.com>
2020-01-06 14:07:19 +0900

    Cleaned up main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,27 +1,17 @@
-use login_screen::options::*;
-use login_screen::options::LoginResult;
-use login_screen::options::abort::abort;
-use login_screen::options::help::help;
-use login_screen::options::signup::signup;
-use login_screen::options::signin::signin;
+use login_screen::LoginResult;
+use login_screen::abort::abort;
+use login_screen::help::help;
+use login_screen::signup::signup;
+use login_screen::signin::signin;
+use login_screen::error::Error;
 
-use login_screen::options::error::Error;
-use std::{io,process};
-use std::io::Write;
-use argonautica::Hasher;
 use promptly::{prompt}; //use promptly::{prompt, prompt_default};
-use reqwest::Response;
-use rpassword::read_password;
-use serde::{Deserialize, Serialize};
+use std::process;
 
 async fn login_screen() -> Result<LoginResult, Error>{
 	loop {
 		print!("\x1B[2J");
-		print!("Hello! Would you like to (R)egister or (S)ign in? ");
-		io::stdout().flush().unwrap();
-		let mut input = String::new();
-		io::stdin().read_line(&mut input)
-				.expect("Failed to read line");
+		let input: String = prompt("Hello! Would you like to (R)egister or (S)ign in? ");
 		match match input.trim().to_uppercase().as_str() {
 				"ABORT" | "EXIT" | "Q" | "QUIT" => abort(),
 				"HELP" | "H" => help(),
