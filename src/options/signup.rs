49ed49957b0fe4d04ef22f4db0e327ac997a6772
Afront <3943720+Afront@users.noreply.github.com>
2020-01-06 22:56:23 +0900

    Added username validation

--- src/options/signup.rs
+++ src/options/signup.rs
@@ -2,8 +2,7 @@ use crate::Result;
 pub use crate::io::{PasswordStep, password_prompt};
 use crate::{hash,LoginResult};
 use addr::{Email, Host};
-use promptly::prompt;
-use reqwest::Response;
+use promptly::{prompt,prompt_default};
 use std::env;
 use serde::{Deserialize, Serialize};
 use validators::email::Email as Validator;
@@ -28,27 +27,56 @@ async fn validate_email(email: &String) -> Result<bool> {
 	}
 }
 
-
-async fn send_json(user_json: String) -> Result<Response> {
+async fn send_json(user_json: String) -> Result<bool> {
 	let client = reqwest::Client::new();
 	let server_url = env::var("SERVER_URL").expect("SERVER_URL must be set");
 
 	println!("{:?}", &user_json);
 
 	let res = client.post(&server_url)
+		//.body(user_json)
 		.body("hi!")
 		.send()
 		.await?;
 
 	println!("{:?}", &res);
 
-	Ok(res)
+	let user_registered = true;
+
+	Ok(user_registered)
 }
+
+async fn is_username_invalid(username: &String) -> Result<bool> {
+	if reqwest::get(&("https://www.purgomalum.com/service/containsprofanity?text=".to_owned() + &username))
+		.await?
+		.text()
+		.await? == "true"{
+			return Ok(true);
+		}
+
+/*
+	let server_url = env::var("SERVER_URL").expect("SERVER_URL must be set");
+	let duplicate_checker = client.post(&server_url)
+		//.body(user_json)
+		.body("hi!")
+		.send()
+		.await?;
+*/
+	Ok(false)
+}
+
+
 
 pub async fn signup() -> Result<LoginResult> {
 	loop {
 		print!("\x1B[2J");
 		let username: String = prompt("Please enter your username");
+		
+		if is_username_invalid(&username).await.unwrap() { //will be turned into a function that checks if someone has the username or not
+			prompt_default("Username taken!", true);
+			continue;
+		}
+
 		let email: String = prompt("Please enter your email");
 
 		if validate_email(&email).await.unwrap() {
@@ -61,9 +89,13 @@ pub async fn signup() -> Result<LoginResult> {
 				};
 				let user_json = serde_json::to_string(&user)?;
 				println!("{:?}", user_json);
-				send_json(user_json).await?;
-				return Ok(LoginResult::SignedUp)
+				if send_json(user_json).await? {
+					return Ok(LoginResult::SignedUp)
+				}
+				prompt_default("The email you entered is already being used. Please enter another email!", true);
+				continue;
 			}	
 		}
+		prompt_default("The email you entered is not valid. Please enter another email!", true);
 	}
 }
