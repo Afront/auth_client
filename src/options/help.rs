19be9d077c6a86b541ff28133aa315719669022d
Afront <3943720+Afront@users.noreply.github.com>
2020-01-07 20:45:19 +0900

    Updated help

--- src/options/help.rs
+++ src/options/help.rs
@@ -2,9 +2,13 @@ use crate::Error;
 use crate::LoginResult;
 
 pub fn help() -> Result<LoginResult, Error> {
-	let something_wrong_happened = false;
-	println!("You need help? I don't think I'm the right person to ask. Try calling someone on your phone.");
+	println!("(H)elp  => Prints this help screen");
+	println!("(Q)uit | Abort | Exit  => Exit the app");
+	println!("(R)egister | Signup | Signup => Sign up to the app");
+	println!("(S)ign in | Signin => Sign in to the app");
 
+	let something_wrong_happened = false;
+	
 	match something_wrong_happened {
 		true => Err(Error::Login(LoginResult::Helped)),
 		false => Ok(LoginResult::Helped)
