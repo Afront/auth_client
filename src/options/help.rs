cd2117ff572351e02ec0db4acf5097b3665fc277
Afront <3943720+Afront@users.noreply.github.com>
2020-01-08 15:47:43 +0900

    Added login to help

--- src/options/help.rs
+++ src/options/help.rs
@@ -5,7 +5,7 @@ pub fn help() -> Result<LoginResult, Error> {
 	println!("(H)elp  => Prints this help screen");
 	println!("(Q)uit | Abort | Exit  => Exit the app");
 	println!("(R)egister | Signup | Signup => Sign up to the app");
-	println!("(S)ign in | Signin => Sign in to the app");
+	println!("(S)ign in | Signin | Login | Log in => Sign in to the app");
 
 	let something_wrong_happened = false;
 	
